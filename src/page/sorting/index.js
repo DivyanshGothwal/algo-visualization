c5f1028f63519922dab3ca0faad084dd23fe1620
Divyansh Gothwal <shotsdivyansh@gmail.com>
2019-08-30 18:28:54 +0530

    changed title bar name

--- src/page/sorting/index.js
+++ src/page/sorting/index.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import { Menu, Icon, Layout, Button } from 'antd';
 
 import { Sorting } from '../../containers';
 
-const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
 
 class SortingPage extends Component {
     render() {
